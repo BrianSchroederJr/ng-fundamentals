d008d2cedb54420a4dfa1f8334d5496e9110e353
bschroeder <bschroeder@fresno.courts.ca.gov>
2019-03-01 10:46:00 -0800

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [
@@ -9,6 +12,9 @@ import { EventsAppComponent } from './events-app.component';
   ],
   declarations: [
     EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent
   ],
   providers: [],
   bootstrap: [EventsAppComponent]
---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './navbar.component.html',
+  styles: [`
+    .nav.navbar-nav {font-size: 15px;}
+    #searchForm {margin-right: 100px;}
+    @media (max-width: 1200px) {#searchForm {display: none}}  /* Hide the searchForm piece if the window gets too small (less than or equal to 1200px) */
+  `]
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
