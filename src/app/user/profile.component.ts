be4824d5f97577aeae80fec2bf8ccc173300ed1c
bschroeder <bschroeder@fresno.courts.ca.gov>
2019-03-07 10:53:27 -0800

    Added validation to Reactive form for User Profile

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,29 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+    em {float: right; color: #E05C65; padding-left: 10px;}
+    .error input {background-color: #E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999 }
+    .error ::-moz-placeholder { color: #999 }
+    .error :-moz-placeholder { color: #999 }
+    .error :ms-input-placeholder { color: #999 }
+
+  `]
 })
 export class ProfileComponent implements OnInit {
 
   profileForm: FormGroup;
+  private mdlFirstName: FormControl;
+  private mdlLastName: FormControl;
 
   constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit() {
-    let mdlFirstName = new FormControl(this.authService.currentUser.firstName);
-    let mdlLastName = new FormControl(this.authService.currentUser.lastName);
+    this.mdlFirstName = new FormControl(this.authService.currentUser.firstName, [Validators.required, Validators.pattern('[a-zA-Z].*')]);
+    this.mdlLastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
     this.profileForm = new FormGroup({
-      firstName: mdlFirstName,
-      lastName: mdlLastName
+      firstName: this.mdlFirstName,
+      lastName: this.mdlLastName
     });
   }
 
   saveProfile(formValues) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-    this.router.navigate(['events']);
+    // Only save profile if the Reactive profile Form is valid
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
+  }
+
+  validateFirstName() {
+    return this.mdlFirstName.valid || this.mdlFirstName.untouched;
+  }
+
+  validateLastName() {
+    return this.mdlLastName.valid || this.mdlLastName.untouched;
   }
 
   cancel() {
